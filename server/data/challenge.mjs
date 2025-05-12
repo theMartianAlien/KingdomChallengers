20bd3666d5a2ab77fbe93c0345cb81eff6aba3f6
Martian <marvs298@yahoo.com>
2025-05-12 08:36:39 -0400

    no missing files

--- server/data/challenge.mjs
+++ server/data/challenge.mjs
@@ -1,16 +1,3 @@
-// export async function getAChallenge(id) {
-
-//     const newChallenger = await Promise.all(
-//         challenges.map(async (item)  => {
-//         const aPlayer = await getAPlayer(item._id);
-//         return {
-//             ...item,
-//             challenger: aPlayer.display_name
-//         }
-//     }))
-//     return await getOneById(TABLE, id);
-// }
-
 import { getAll, getOneBy, getOneById, writeOne, } from "../util/mongo.mjs";
 import { getAccountById } from "./auth.mjs";
 import { getAPlayer } from "./players.mjs";
@@ -19,14 +6,9 @@ const TABLE = "challenges";
 
 export async function getAllChallenges() {
     const challenges = await getAll(TABLE);
-    console.log(challenges);
-    console.log("get ALL inside ")
     const newChallenges = await Promise.all(challenges.map(async (challenge) => {
-        console.log(challenge);
         const aPlayer = await getAccountById(challenge.issuer);
-        console.log(aPlayer);
         const challenger = await getAPlayer(aPlayer.player_id);
-        console.log(challenger);
         return {
             ...challenge,
             challenger: challenger.display_name
