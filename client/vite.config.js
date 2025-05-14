25fe113e5974192e354d6da2328b0ffceec70e2c
Martian <marvs298@yahoo.com>
2025-05-13 20:35:54 -0400

    clean up npm

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,17 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import flowbiteReact from "flowbite-react/plugin/vite";
-import sass from 'sass'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), flowbiteReact()],
-    css: {
-    preprocessorOptions: {
-      scss: {
-        implementation: sass,
-      },
-    },
-  },
   base: process.env.VITE_BASE_PATH //|| '/KingdomChallengers/client'
 })
